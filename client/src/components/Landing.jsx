14c1a7be1ee34ad2a0b30fa964273f88c8a9457d
Santiago <srueda9331@gmail.com>
2022-09-13 03:59:47 -0500

    renamed main for landing and removing title

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import '../styles/main.css'
+import '../styles/Landing.css'
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,15 +7,12 @@ export default function Main(){
 
   return (
     
-    
     <div className='background-main'>
-      <h3 className='title'>Welcome To!</h3>
-      <h3 className='initial-text'>The Site Where You Could Find Some Info About Pokemons!</h3>
+      <h3 className='initial-text'>Welcome To The Site Where You Could Find Some Info About Pokemons!</h3>
       <Link to='/home'>
         <button className='main-button'>Go ahead!</button>
       </Link>
     </div>
   
-   
   )
 }
