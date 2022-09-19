6dc26560f29b242c27d022be655e64fd73eb864a
Santiago <srueda9331@gmail.com>
2022-09-18 20:21:13 -0500

    added two images to the searchBar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,7 +1,10 @@
+import loading from '../image/Loading1.gif'
+import pokemon from '../image/pokemon1.png'
+import pikachu from '../image/pikachu1.png'
 import '../styles/Home.css'
 import React, { useState }  from "react";
 import { useDispatch } from "react-redux";
-import { searchPokemon, getPokemons } from "../actions";
+import { searchPokemon } from "../actions";
 
 
 export function SearchBar(){
@@ -14,7 +17,6 @@ export function SearchBar(){
   function handleInputChange(e){
     e.preventDefault()
     setName(e.target.value)
-    console.log(name)
   }
 
   async function handleSubmit(e){
@@ -24,54 +26,52 @@ export function SearchBar(){
       setMessage('')
       setImg('d')
       
-      d = await dispatch(searchPokemon(name))
-      if(d.payload === 'Not found'){
-        alert('Sorry, this pokemon cannot be found')
-        setName('')
-      }
+      await dispatch(searchPokemon(name))
+      // if(d.payload === 'Not found'){
+      //   // alert('Sorry, this pokemon cannot be found')
+      //   setMessage('Sorry, this pokemon cannot be found')
+      // }
       
       setName('')    
       setMessage('')
       setImg('')
 
     } if(!name){
-      alert('Write a name please') 
+      setMessage('Write a name please') 
       setName('') 
     }
   
   }
 
   return (
-    <div className='contain'>
-      {/* <div> */}
-        
-      {/* </div> */}
+    <div>
+   
       <div className='nav-bar'>
-      
+      <div>
+        <img style={{height:'77px', width:'184px', padding:'10px', marginLeft:'50px'}}src={pokemon} alt="brand" />
+      </div>
         <input className='bar' type='text' onChange={(e) => handleInputChange(e)} placeholder='Search by name only' />
         <button className='bar-button' type='submit' onClick={(e) => handleSubmit(e)}>Search</button>
         {
-      message.length > 0 && (
-        <div >
-          <p className="searchBar1">{message}</p>
-        </div>
-      )
-    }
-    {
-      img === 'd' && (
-        <div onChange={handleInputChange}>
-          <p className='img-search'>Loading...</p>
-        </div> 
-      )
-    }   
+          img === 'd' && (
+            <div onChange={handleInputChange}>
+              {/* <span className='img-search'>Loading</span> */}
+              <span><img style={{width: '64px', marginLeft:'7px', marginTop:'45px'}} alt='load-gif' src={loading} /></span>
+            </div> 
+          )
+        }   
+        {
+          message.length > 0 && (
+            <div>
+              <span className="no-name">{message}</span>
+            </div>
+          )
+        }
+        <img style={{padding:'2px', marginLeft:'50px', position:'fixed', marginLeft:'1100px'}}src={pikachu} alt="pikachu" />
+
         </div>
         
     </div>
-    
-      
-    
-    
-   
     
   )
  
