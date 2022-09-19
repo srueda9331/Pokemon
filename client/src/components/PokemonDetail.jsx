4806868f91044892ce5e07450f0cac5e81e8d680
Santiago <srueda9331@gmail.com>
2022-09-18 20:03:17 -0500

    the appearence of the componentDetail has changed

--- client/src/components/PokemonDetail.jsx
+++ client/src/components/PokemonDetail.jsx
@@ -1,4 +1,6 @@
 import '../styles/pokemonDetail.css'
+import circle from '../image/circle1.png'
+import circleWhite from '../image/circulo-blanco.png'
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -7,7 +9,6 @@ import axios from "axios";
 
 
 export function PokemonDetail(){
-  // const dispatch = useDispatch();
   const history = useHistory()
   const [pokemon, setPokemon] = useState(null)
   let {id} = useParams()
@@ -16,50 +17,47 @@ export function PokemonDetail(){
   useEffect(() => {
     axios.get('http://localhost:3001/pokemons/' + id )
     .then(res => {
-      if(!res.data.id){
-        alert("The pokemon youŕe looking for doesn't exist")
+      setPokemon(res.data)
+    })
+    .catch(error => {
+      if(error.name === 'AxiosError'){
+        alert("Sorry, the pokemon you're looking for doesn't exist")
         history.push('/home')
         setPokemon(null)
       }
-      setPokemon(res.data)
     })
     return () => {
       setPokemon(null)
     }
-  }, [])
-  
-  // useEffect(() => {
-  //   dispatch(getPokemonDetail(props.match.params.id));
-  //   return () => dispatch(cleanDetail());
-  // }, []);
-
-  // const pokemon = useSelector((state) => state.detail)
-  
+  }, [history, id])
+    
   
   return (
     <div>
-      
       {
         pokemon? 
-        
         <div className='alldetail'>
           <div className="detail1">
             <div className='pokemond1'>
-            <h2>Id: {pokemon.id}</h2>
+            <h2>Id: <span style={{color:'black', fontSize:'26px'}}>{pokemon.id}</span></h2>
             
-            <h2 className='title2-det'>Hi, I'm {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)},</h2>
-            <h3 className='t'>Pleased to meet you</h3>
+            <h2 className='title2-det'>Hi, I'm <span style={{color:'black', fontSize:'28px'}}>{pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</span>,</h2>
+           
+            <h3 style={{marginTop:'32px'}}>Pleased to meet you,</h3>
             <h3>All you need to know about me is on the right</h3>
             
             <img className='img-detail' src={pokemon.img? pokemon.img : pokemon.image} alt="Myimage" />
             </div>
           </div>
+
           <div className="detail2">
+          <img style={{height: '151px', marginLeft:'-270px', marginBottom:'130px', zIndex:-1}}src={circle} />
+          <img style={{height: '128px', marginLeft:'-139px', marginBottom:'141px', zIndex:1}}src={circleWhite} />
             <div className='pokemond2'>
               <h3>Life points: {pokemon.hp}</h3>
               <h3>Attack: {pokemon.attack}</h3>
               <h3>Defense: {pokemon.defense}</h3>
-              <h3>Speed: {pokemon.speed}</h3>
+              <h3>Speed: {pokemon.speed} dm/sec</h3>
               {
                 pokemon?.typeOne? <h3>Type One: {pokemon.typeOne[0].toUpperCase() + pokemon.typeOne.slice(1)}</h3> : false
               }
@@ -75,8 +73,8 @@ export function PokemonDetail(){
           
 
               
-              <h3>Height: {pokemon.height}</h3>
-              <h3>Weight: {pokemon.weight}</h3>
+              <h3>Height: {pokemon.height > 9? pokemon.height.toString()[0] + '.' + pokemon.height.toString()[1]: '0.' + pokemon.height } mts</h3>
+              <h3>Weight: {pokemon.weight * 10 ** -1} Kg</h3>
               <Link to='/home'>
                 <button className='button-detail'>Get back</button>
             </Link> 
@@ -85,9 +83,8 @@ export function PokemonDetail(){
         </div>
         : 
         <div>
-        <h3>loading...</h3> <br />
-       
-        <img src="https://static.wixstatic.com/media/20abc5_e58061f333744c2899c375ec7f024eb3~mv2.gif" alt="" />
+        <h3 id='load-detail'>Loading...</h3> <br />
+        <img style={{marginTop:'-200px'}} src="https://static.wixstatic.com/media/20abc5_e58061f333744c2899c375ec7f024eb3~mv2.gif" alt="poke-load" />
         </div>
       }
 
